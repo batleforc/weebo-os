1b5252ad5f125b192f5d3f6b9bd86cb6be6a0e74
Max Batleforc <maxleriche.60@gmail.com>
2026-03-09 13:31:24 +0100

    feat: pinrpmostree

---
+++ files/scripts/binary/pinrpmostree.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright 2025-2026 The Secureblue Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Taken from https://github.com/wayblueorg/wayblue/blob/live/files/scripts/pinrpmostree.sh
+
+set -oue pipefail
+
+RPM_OSTREE_VERSION="2025.12-1"
+
+dnf install -y --from-repo=updates-archive \
+  "rpm-ostree-${RPM_OSTREE_VERSION}.fc${OS_VERSION}" \
+  "rpm-ostree-libs-${RPM_OSTREE_VERSION}.fc${OS_VERSION}"
