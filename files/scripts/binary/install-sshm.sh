7c8ac87aaacb4c20d04a8df3143c51bb49d0d136
Max Batleforc <maxleriche.60@gmail.com>
2026-07-28 00:42:05 +0200

    feat: add sshm gui

--- files/scripts/binary/install-sshm.sh
+++ files/scripts/binary/install-sshm.sh
@@ -3,8 +3,7 @@
 set -oue pipefail
 
 # Install SSHM
-
-SSHM_VERSION="1.5.1"
+SSHM_VERSION="2.0.0"
 TEMP_DIR="$(mktemp -d)"
 
 echo "Installing SSHM ${SSHM_VERSION} from prebuilt binary..."
@@ -20,6 +19,12 @@ tar -xzf sshm.tar.gz
 # Install the binary to /usr/bin
 mkdir -p /usr/bin
 install -m 755 sshm /usr/bin/sshm
+
+# Download the GUI package  for x86_64 Linux
+curl -L "https://github.com/Sn0wAlice/sshm/releases/download/v${SSHM_VERSION}/sshm-${SSHM_VERSION}-1.x86_64.rpm" -o sshm-gui.rpm
+
+# Install the GUI package using rpm
+dnf install -y sshm-gui.rpm
 
 # Clean up
 cd -
